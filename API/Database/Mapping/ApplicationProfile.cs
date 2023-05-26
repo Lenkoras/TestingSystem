5e497afa51762e152b703547b3b657c46e477975
Ivan Sakharuk <ivansakharuk@gmail.com>
2023-05-26 19:37:12 +0300

    Added AutoMapper and configured Profile.

---
+++ API/Database/Mapping/ApplicationProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Database.Models;
+using Shared.Models;
+
+namespace Database.Mapping
+{
+    public class ApplicationProfile : Profile
+    {
+        public ApplicationProfile()
+        {
+            CreateMap<QuestionAnswer, QuestionAnswerShort>();
+            CreateMap<TestQuestion, TestQuestionShort>();
+            CreateMap<Test, UserTestShort>();
+        }
+    }
+}
