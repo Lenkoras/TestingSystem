ba71d079f396460bdced93a3cb6a5b7f606e32e6
Ivan Sakharuk <ivansakharuk@gmail.com>
2023-05-26 19:36:03 +0300

    Added class for default access token values.

---
+++ API/Auth/Tokens/AccessTokenDefaults.cs
@@ -0,0 +1,9 @@
+﻿namespace Auth.Tokens
+{
+    public static class AccessTokenDefaults
+    {
+        public static readonly string AccessTokenKey = "Access-Token";
+
+        public static readonly string ProtectorPurpouse = nameof(AccessTokenDefaults);
+    }
+}
