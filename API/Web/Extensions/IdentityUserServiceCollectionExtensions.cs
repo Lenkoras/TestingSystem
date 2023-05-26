5d62c29260a710cc3a0bb6bddb0ec70bb76e678c
Ivan Sakharuk <ivansakharuk@gmail.com>
2023-05-26 22:22:49 +0300

    Added authenticatation extensions.

---
+++ API/Web/Extensions/IdentityUserServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Database;
+using Database.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Web.Extensions
+{
+    public static class IdentityUserServiceCollectionExtensions
+    {
+        public static IdentityBuilder AddIdentityUser(this IServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            /// IdentityBuilder
+            return services.AddIdentityCore<User>(ConfigureIdentity)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+        }
+
+        private static void ConfigureIdentity(IdentityOptions options)
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireDigit = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireLowercase = true;
+        }
+    }
+}
---
+++ API/Web/Extensions/JwtAuthenticationServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Auth;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Web.Extensions
+{
+    public static class JwtAuthenticationServiceCollectionExtensions
+    {
+        public static AuthenticationBuilder AddJwtAuthentication(this IServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            return services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer((JwtBearerOptions options) =>
+                {
+                    options.TokenValidationParameters = CreateTokenValidationParameters();
+                });
+        }
+
+        private static TokenValidationParameters CreateTokenValidationParameters() =>
+            new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = AuthOptions.Issuer,
+                IssuerSigningKey = new SymmetricSecurityKey(AuthOptions.GetEncryptionKeyBytes()),
+            };
+    }
+}
