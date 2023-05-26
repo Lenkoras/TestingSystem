9fdbc0af0592841eedc6fc2c6ada1ebffb4fdca0
Ivan Sakharuk <ivansakharuk@gmail.com>
2023-05-26 21:32:41 +0300

    Added implementation of IAuthorizationService.

---
+++ API/Logic/Services/ApplicationAuthorizationService.cs
@@ -0,0 +1,22 @@
+﻿using Auth;
+using Database.Models;
+using Database.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Logic.Services
+{
+    public class ApplicationAuthorizationService : IAuthorizationService<User>
+    {
+        private readonly IRepositoryWrapper repositoryWrapper;
+
+        public ApplicationAuthorizationService(IRepositoryWrapper repositoryWrapper)
+        {
+            this.repositoryWrapper = repositoryWrapper;
+        }
+
+        public async ValueTask<User?> AuthorizeAsync(string userName, string password)
+        {
+            return await repositoryWrapper.Users.FirstOrDefaultAsync(user => user.UserName == userName);
+        }
+    }
+}
