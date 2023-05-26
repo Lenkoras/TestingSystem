477011e2fb64e35ab48ccaa5b534886e2ba6b129
Ivan Sakharuk <ivansakharuk@gmail.com>
2023-05-26 19:33:44 +0300

    Added authorization and authentication interfaces.

---
+++ API/Auth/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+﻿namespace Auth
+{
+    public interface IAuthenticationService<TUser>
+        where TUser : IUser<Guid>
+    {
+        ValueTask AuthenticateAsync(TUser user);
+    }
+}
---
+++ API/Auth/IAuthorizationService.cs
@@ -0,0 +1,8 @@
+﻿namespace Auth
+{
+    public interface IAuthorizationService<TUser>
+        where TUser : IUser<Guid>
+    {
+        ValueTask<TUser?> AuthorizeAsync(string userName, string password);
+    }
+}
