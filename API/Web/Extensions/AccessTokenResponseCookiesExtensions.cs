4183ad6f98063df4e614112b1ffb4cdda53668ed
Ivan Sakharuk <ivansakharuk@gmail.com>
2023-05-27 19:57:42 +0300

    Added IResponseCookies extension.
    Added an extension for IResponseCookies to remove the Access-Token in client side.

---
+++ API/Web/Extensions/AccessTokenResponseCookiesExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Auth.Tokens;
+
+namespace Web.Extensions
+{
+    public static class AccessTokenResponseCookiesExtensions
+    {
+        public static void DeleteAccessToken(this IResponseCookies cookies)
+        {
+            ArgumentNullException.ThrowIfNull(cookies);
+
+            cookies.Delete(
+                AccessTokenDefaults.AccessTokenKey,
+                new CookieOptions()
+                {
+                    HttpOnly = true,
+                    Secure = true
+                }
+            );
+        }
+    }
+}
