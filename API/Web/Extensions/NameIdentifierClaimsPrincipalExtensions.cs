6b3fa8f2259e5dfffb1df38303ffe2f27f0f9980
Ivan Sakharuk <ivansakharuk@gmail.com>
2023-05-28 04:12:57 +0300

    Added extension for ClaimsPrincipal.
    Shorthand for getting parsed user id from claims.

---
+++ API/Web/Extensions/NameIdentifierClaimsPrincipalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+
+namespace Web.Extensions
+{
+    public static class NameIdentifierClaimsPrincipalExtensions
+    {
+        public static bool TryGetId(this ClaimsPrincipal claimsPrincipal, out Guid id)
+        {
+            ArgumentNullException.ThrowIfNull(claimsPrincipal);
+
+            string? textId = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (textId is not null && Guid.TryParse(textId, out id))
+            {
+                return true;
+            }
+            id = default;
+            return false;
+        }
+    }
+}
